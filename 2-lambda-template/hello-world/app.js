a2d7dd46a44e25f42fbb401421bae8995a36d0fd
edward.carmody <edward.carmody@gmail.com>
2023-10-26 14:34:11 -0400

    updates

--- 2-lambda-template/hello-world/app.js
+++ 2-lambda-template/hello-world/app.js
@@ -1,9 +1,26 @@
 
-exports.lambdaHandler = async (event, context) => {
-  return {
-    'statusCode': 200,
-    'body': JSON.stringify({
-        message: 'hello world'
-    })
-  }
+const { exec } = require('child_process')
+
+const execPromise = async (command) => {
+	console.log(command)
+	return new Promise((resolve, reject) => {
+		const ls = exec(command, function (error, stdout, stderr) {
+		  if (error) {
+		    console.log('Error: ', error)
+		    reject(error)
+		  }
+		  console.log('stdout: ', stdout);
+		  console.log('stderr: ' ,stderr);
+		})
+		
+		ls.on('exit', function (code) {
+		  console.log('Finished: ', code);
+		  resolve()
+		})
+	})
+}
+
+// The Lambda handler
+exports.lambdaHandler = async function (eventObject, context) {
+	await execPromise('ls -l /mnt/efs')
 }
